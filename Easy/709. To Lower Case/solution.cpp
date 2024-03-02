b96fb8ba1ecbe3d988fc2af062e6a302170eff36
uttamkn <uttamkn15@gmail.com>
2024-03-02 20:31:31 +0530

    add q709

---
+++ Easy/709. To Lower Case/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.size(); i++) {
+            if((s[i]>64)&&(s[i]<91)) s[i]=s[i]+32;
+        }
+
+        return s;
+    }
+};
