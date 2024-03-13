688f2466ae82348e3514e1b6beb03ea624a7fbf9
uttamkn <uttamkn15@gmail.com>
2024-03-13 17:35:00 +0530

    trace q1171

--- Medium/1171. Remove Zero Sum Consecutive Nodes from Linked List/solution.cpp
+++ Medium/1171. Remove Zero Sum Consecutive Nodes from Linked List/solution.cpp
@@ -1,20 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include<iostream>
+#include<stdlib.h>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
         int prefix_sum = 0;
-        std::unordered_map<int, ListNode*> prefix_sums;
+        unordered_map<int, ListNode*> prefix_sums;
         prefix_sums[0] = dummy;
         ListNode* current = head;
 
@@ -38,3 +43,20 @@ public:
         return dummy->next;
     }
 };
+
+int main() {
+    Solution ob;
+    ListNode* n1 = new ListNode(4);
+    ListNode* n2 = new ListNode(-3, n1);
+    ListNode* n3 = new ListNode(3, n2);
+    ListNode* n4 = new ListNode(2, n3);
+    ListNode* n5 = new ListNode(1, n4);
+
+    ListNode* tmp = ob.removeZeroSumSublists(n5);
+
+    while(tmp!=nullptr) {cout << tmp->val << " ";
+    cout << endl;
+    tmp=tmp->next;
+    } 
+    return 0;
+}
