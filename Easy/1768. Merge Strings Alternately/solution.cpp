644bfe6321af24a3feefb96084ea4626c01aeccb
uttamkn <uttamkn15@gmail.com>
2024-05-06 19:07:45 +0530

    add q1768

---
+++ Easy/1768. Merge Strings Alternately/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result="";
+        int i=0, j=0;
+        while(i<word1.size() || j<word2.size()) {
+            if(i<word1.size()) {
+                result+=word1[i++];
+            }
+
+            if(j<word2.size()) {
+                result+=word2[j++];
+            }
+        }
+
+        return result;
+    }
+};
