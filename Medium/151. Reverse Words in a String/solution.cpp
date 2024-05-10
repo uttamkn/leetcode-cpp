63c6245da22066e8366e5aad6ddffe647557cd6f
uttamkn <uttamkn15@gmail.com>
2024-05-10 17:49:43 +0530

    add O(1) sol for q151

--- Medium/151. Reverse Words in a String/solution.cpp
+++ Medium/151. Reverse Words in a String/solution.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<strstream>
+#include<sstream>
 #include<stack>
 #include<string>
 using namespace std;
@@ -25,5 +25,39 @@ public:
 
         res.erase(res.size()-1);
         return res;
+    }
+};
+
+//O(1) space complexity
+class Solution {
+public:
+    string reverseWords(string s) {
+        s=trim(s);
+        reverse(s.begin(), s.end());
+        istringstream iss(s);
+        string token;
+        size_t pos=0;
+        while(getline(iss, token, ' ')) {
+            if(!token.empty()) {
+                pos=s.find(token, pos);
+                reverse(token.begin(), token.end());
+                s.replace(pos, token.size(), token);
+                pos++;
+            }
+            else { //if token is a null character (consecutive blank spaces)
+                s.erase(s.find("  "), 1);
+            }
+        }
+        return s;
+    }
+
+private:
+    string trim(const string& str) {
+        size_t first = str.find_first_not_of(' ');
+        if (string::npos == first) {
+            return "";
+        }
+        size_t last = str.find_last_not_of(' ');
+        return str.substr(first, (last - first + 1));
     }
 };
