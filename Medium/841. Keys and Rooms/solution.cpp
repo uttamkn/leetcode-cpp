7bab1cb2c50f0c64652213d4a20d38e895c872a2
uttamkn <uttamkn15@gmail.com>
2024-10-18 18:13:41 +0530

    add q841

--- Medium/841. Keys and Rooms/solution.cpp
+++ Medium/841. Keys and Rooms/solution.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <numeric>
 using namespace std;
 
 class Solution {
@@ -6,13 +7,22 @@ public:
   bool canVisitAllRooms(vector<vector<int>> &rooms) {
     vector<int> visited(rooms.size(), 0);
     visited[0] = 1;
+    stack<int> stk;
+    stk.push(0);
 
-    for (vector<int> keys : rooms) {
-      for (int key : keys) {
-        if (visited[key] == 0)
+    while (!stk.empty()) {
+      int i = stk.top();
+      stk.pop();
+
+      for (int key : rooms[i]) {
+        if (visited[key] == 0) {
           visited[key] = 1;
+          stk.push(key);
+        }
       }
     }
+
+    return (rooms.size() == accumulate(visited.begin(), visited.end(), 0));
   }
 };
 
