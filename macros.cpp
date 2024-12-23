69834b6ed80e9d387c285e1bae14dd79db7cb488
uttamkn <uttamkn15@gmail.com>
2024-12-23 20:57:37 +0530

    add some useful macros

---
+++ macros.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+
+#define dbg(x) cerr << #x << " = " << x << endl;
+#define dbg_vec(v) cerr << #v << " = "; for (auto x : v) cerr << x << " "; cerr << endl;
+#define dbg_pair(p) cerr << #p << " = {" << p.first << ", " << p.second << "}" << endl;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define ceil(a, b) ((a + b - 1) / b)
+
+#define sz(x) (int)(x).size() 
+#define vi vector<int>
+#define pii pair<int, int>
+#define vvi vector<vector<int>>
+#define umap unordered_map
+#define uset unordered_set
+
+#define imin INT_MIN
+#define imax INT_MAX
+#define bankai cout << "Katen Kyokotsu Karamatsu Shinjuu" << endl;
+
+int main() {
+  fast_io;
+}
