c9be1c725b508772a3e1dc12e6baf11f30c3299d
uttamkn <uttamkn15@gmail.com>
2024-12-27 22:03:49 +0530

    update q22

--- Medium/22. Generate Parentheses/solution.cpp
+++ Medium/22. Generate Parentheses/solution.cpp
@@ -4,37 +4,40 @@ class Solution {
 public:
   vector<string> generateParenthesis(int n) {
     vector<string> res;
-    f(2 * n, res, "");
+    f(n, res, "", 0, 0);
     return res;
   }
 
-  bool is_valid(const string combination) {
-    int open = 0, close = 0;
-
-    for (const char c : combination) {
-      if (c == '(')
-        open++;
-      else
-        close++;
-
-      if (close > open)
-        return false;
-    }
-
-    return open == close;
-  }
+  // bool is_valid(const string combination) {
+  //   int open = 0, close = 0;
+  //
+  //   for (const char c : combination) {
+  //     if (c == '(')
+  //       open++;
+  //     else
+  //       close++;
+  //
+  //     if (close > open)
+  //       return false;
+  //   }
+  //
+  //   return open == close;
+  // }
 
 private:
-  void f(int size, vector<string> &res, string cur_combination) {
-    if (size == 0) {
-      if (is_valid(cur_combination))
-        res.push_back(cur_combination);
+  void f(int size, vector<string> &res, string cur_combination, int open, int close) {
+    if (open == close && open == size) {
+      res.push_back(cur_combination);
       return;
     }
 
-    f(size-1, res, cur_combination + "(");
-    if (sz(cur_combination) != 0)
-      f(size-1, res, cur_combination + ")");
+    if(open<size) {
+      f(size, res, cur_combination+"(", open+1, close);
+    }
+
+    if(close < open) {
+      f(size, res, cur_combination+")", open, close+1);
+    }
   }
 };
 
