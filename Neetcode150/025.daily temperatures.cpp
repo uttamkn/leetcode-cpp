6551d2bd2e3780fe0965b03d5125f6f2b290427e
uttamkn <uttamkn15@gmail.com>
2025-03-22 17:42:35 +0530

    neetcode 25

--- Neetcode150/025.daily temperatures.cpp
+++ Neetcode150/025.daily temperatures.cpp
@@ -4,18 +4,21 @@ using namespace std;
 class Solution {
 public:
   vector<int> dailyTemperatures(vector<int> &temperatures) {
+    stack<pair<int, int>> stk;
     int n = temperatures.size();
+
     vector<int> ans(n);
 
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = i + 1; j < n; ++j) {
-        if (temperatures[j] > temperatures[i]) {
-          ans[i] = j - i;
-          break;
-        }
+    for (int i = n - 1; i >= 0; --i) {
+      while (!stk.empty() && stk.top().first <= temperatures[i]) {
+        stk.pop();
       }
-    }
 
+      if (!stk.empty()) {
+        ans[i] = stk.top().second - i;
+      }
+      stk.push({temperatures[i], i});
+    }
     return ans;
   }
 };
